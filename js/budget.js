68a08cfceb62961ca426e5aa2fdbed9dcf4781b1
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-08-23 00:54:59 +0600

    first commit

---
+++ js/budget.js
@@ -0,0 +1,46 @@
+
+// common function for purches from the id 
+function tagStringToNumber(input) {
+    const elementt = document.getElementById(input);
+    const elementtString = elementt.innerText
+    const elementtStringToNumber = parseFloat(elementtString);
+    return elementtStringToNumber;
+}
+
+// text jog biyog sheshe gaiga mot rakha 
+function textInputt(oldInputt, newInputt) {
+    const oldInputttt = document.getElementById(oldInputt);
+    oldInputttt.innerText = newInputt
+}
+
+
+function totalCosts() {
+    const playerExpensesCost = tagStringToNumber('player-expenses')
+    const managerExpensesCost = tagStringToNumber('manager-expenses')
+    const coachExpensesCost = tagStringToNumber('coach-expenses')
+
+    const totalCost = playerExpensesCost + managerExpensesCost + coachExpensesCost;
+
+    textInputt('total-player-expenses', totalCost)
+}
+
+const calculate = document.getElementById('calculate');
+calculate.addEventListener('click', function () {
+
+
+    const playerNumber = tagStringToNumber('player-select')
+
+    const playerExpensess = 1000 * playerNumber;
+
+
+    textInputt('player-expenses', playerExpensess)
+
+    // totalCosts()
+
+})
+
+const totalll = document.getElementById('total');
+totalll.addEventListener('click', function () {
+    totalCosts()
+
+})
---
+++ js/select.js
@@ -0,0 +1,54 @@
+
+
+// // select player 0 er id dhorlam 
+const playerTotsl = document.getElementById('player-select');
+const tableBody = document.getElementById('table');
+
+// nam bosanor jonno array declar korlam 
+const playerArray = [];
+
+
+function playerItems(playerA) {
+
+    tableBody.innerHTML = "";
+    for (let index = 0; index < playerA.length; index++) {
+        const item = playerA[index];
+        const playersNamess = item.playerNameH1
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${index + 1}</th>
+        <td>${playersNamess}</td>
+        `;
+        tableBody.appendChild(tr)
+    }
+}
+
+
+function selectP(element) {
+    element.disabled = true;
+    const playerNameH1 = element.parentNode.parentNode.children[0].innerText;
+
+    // array er length 
+    const playerLenthhh = playerArray.length;
+    // create an objct 
+    const playerObject = {
+        no: playerLenthhh + 1,
+        playerNameH1: playerNameH1
+    }
+
+    if (playerLenthhh <= 4) {
+    }
+    else {
+        alert('you can not select more than 5 players')
+        element.disabled = false;
+        return;
+    }
+
+    // // object ke array te push korlam 
+    playerArray.push(playerObject);
+
+    // // array er length  er man select player e bosiye dilam
+    playerTotsl.innerText = playerLenthhh + 1;
+
+    playerItems(playerArray);
+}
